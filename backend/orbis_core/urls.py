8d479a1d9b39561ec9aba280ffe85a2156d6d6e0
allanperez08 <allanperezajanel@gmail.com>
2026-03-19 17:45:50 -0600

    feat: setup routing architecture using bounded contexts

--- backend/orbis_core/urls.py
+++ backend/orbis_core/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for orbis_core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# Urls principal del proyecto, que incluye las rutas de cada aplicación.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/communications/', include('communications.urls')),
+    path('api/execution-results/', include('execution_results.urls')),
+    path('api/request-assignment/', include('request_assignment.urls')),
+    path('api/schedule-availability/', include('schedule_availability.urls')),
+    path('api/security/', include('security.urls')),
+    path('api/tutors-evaluators/', include('tutors_evaluators.urls')),
 ]
