24f08b21ce331213d3b77f3fc64fe3b1bff66d03
FranzFQ <quemejavi@gmail.com>
2026-03-20 16:36:04 -0600

    feat(backend): Split the database orbis into bounded context apps

--- backend/execution_results/models.py
+++ backend/execution_results/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from tutors_evaluators.models_tutors_evaluators import Student
+
+
+class Evaluation(models.Model):
+    id = models.IntegerField(primary_key=True)
+    studentid = models.ForeignKey(
+        Student,
+        models.DO_NOTHING,
+        db_column='studentId'
+    )  # Field name made lowercase.
+    date = models.DateField(blank=True, null=True)
+    hour = models.TimeField(blank=True, null=True)
+    classroom = models.CharField(max_length=4, blank=True, null=True)
+    building = models.CharField(max_length=20, blank=True, null=True)
+    evaluationstatus = models.BooleanField(db_column='evaluationStatus', blank=True, null=True)  # Field name made lowercase.
+    state = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Evaluation'
+
+
+class Result(models.Model):
+    id = models.IntegerField(primary_key=True)
+    evaluationid = models.ForeignKey(
+        Evaluation,
+        models.DO_NOTHING,
+        db_column='evaluationId'
+    )  # Field name made lowercase.
+    score = models.FloatField(blank=True, null=True)
+    observation = models.TextField(blank=True, null=True)
+    state = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Result'
--- backend/request_assignment/models.py
+++ backend/request_assignment/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+from execution_results.models_execution_results import Evaluation
+from tutors_evaluators.models_tutors_evaluators import Evaluator, Mentor, Studygroup
+
+
+class Private(models.Model):
+    id = models.IntegerField(primary_key=True)
+    evaluationid = models.ForeignKey(
+        Evaluation,
+        models.DO_NOTHING,
+        db_column='evaluationId'
+    )  # Field name made lowercase.
+    mentorid = models.ForeignKey(
+        Mentor,
+        models.DO_NOTHING,
+        db_column='mentorId'
+    )  # Field name made lowercase.
+    state = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Private'
+
+
+class PrivateEvaluator(models.Model):
+    pk = models.CompositePrimaryKey('privateid', 'evaluatorid')
+    privateid = models.ForeignKey(
+        Private,
+        models.DO_NOTHING,
+        db_column='privateId'
+    )  # Field name made lowercase.
+    evaluatorid = models.ForeignKey(
+        Evaluator,
+        models.DO_NOTHING,
+        db_column='evaluatorId'
+    )  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Private-Evaluator'
+
+
+class PrivateStudygroup(models.Model):
+    pk = models.CompositePrimaryKey('privateid', 'studygroupid')
+    privateid = models.ForeignKey(
+        Private,
+        models.DO_NOTHING,
+        db_column='privateId'
+    )  # Field name made lowercase.
+    studygroupid = models.ForeignKey(
+        Studygroup,
+        models.DO_NOTHING,
+        db_column='studyGroupId'
+    )  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Private-StudyGroup'
+
+
+class Terna(models.Model):
+    id = models.IntegerField(primary_key=True)
+    evaluationid = models.ForeignKey(
+        Evaluation,
+        models.DO_NOTHING,
+        db_column='evaluationId'
+    )  # Field name made lowercase.
+    mentorid = models.ForeignKey(
+        Mentor,
+        models.DO_NOTHING,
+        db_column='mentorId'
+    )  # Field name made lowercase.
+    state = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Terna'
