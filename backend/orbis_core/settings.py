2b7477ef300b615d1fc532364bf6b5cc1fdafa74
FranzFQ <quemejavi@gmail.com>
2026-04-16 22:20:38 -0600

    feat(backend): Crearted a simple CRUD for orbis database

--- backend/orbis_core/settings.py
+++ backend/orbis_core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     #third party
     'rest_framework',
+    'django_filters',
     'corsheaders',
 
     #apps
@@ -140,3 +141,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
