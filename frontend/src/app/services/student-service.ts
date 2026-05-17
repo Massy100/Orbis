ac5dcf926736374392bfa8d389407a06574f06a1
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2026-05-16 18:06:28 -0600

    feat(students): I create students-management view

--- frontend/src/app/services/student-service.ts
+++ frontend/src/app/services/student-service.ts
@@ -11,5 +11,79 @@ export const studentService = {
         if (!res.ok) return null;
 
         return res.json();
+    },
+
+    async getStudents() {
+        const res = await fetch(
+            `${API}students/`
+        )
+
+        if (!res.ok) {
+            throw new Error(
+                "Error obteniendo estudiantes"
+            )
+        }
+
+        return res.json()
+    },
+    async createStudent(data: {
+        name: string
+        carne: string
+    }) {
+        const res = await fetch(
+            `${API}students/`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type":
+                        "application/json"
+                },
+                body: JSON.stringify(data)
+            }
+        )
+
+        return {
+            ok: res.ok,
+            data: await res.json()
+        }
+    },
+
+    async updateStudent(
+        id: number,
+        data: {
+            name: string
+            carne: string
+        }
+    ) {
+        const res = await fetch(
+            `${API}students/${id}/`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type":
+                        "application/json"
+                },
+                body: JSON.stringify(data)
+            }
+        )
+
+        return {
+            ok: res.ok,
+            data: await res.json()
+        }
+    },
+
+    async deactivateStudent(id: number) {
+        const res = await fetch(
+            `${API}students/${id}/deactivate/`,
+            {
+                method: "PATCH"
+            }
+        )
+
+        return {
+            ok: res.ok,
+            data: await res.json()
+        }
     }
 };
