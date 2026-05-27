11aa5b9961a52449c6d8dc1ea98ae74ca013de34
FranzFQ <quemejavi@gmail.com>
2026-05-26 19:51:52 -0600

    fix(global-api): changed name of env variable

--- frontend/src/app/services/global-api-url.js
+++ frontend/src/app/services/global-api-url.js
@@ -1,3 +1,3 @@
-const GLOBAL_API_URL = process.env.API_URL || "http://localhost:8001/api/";
+const GLOBAL_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8001/api/";
 
 export default GLOBAL_API_URL;
